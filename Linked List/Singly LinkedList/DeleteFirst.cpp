ef79a55276b2f0e5c55c3009f38e2bc33626c3e4
Deepak Chauhan <deepakchauhan6141@gmail.com>
2024-12-26 18:52:53 +0530

    Delete at first

---
+++ Linked List/Singly LinkedList/DeleteFirst.cpp
@@ -0,0 +1,48 @@
+//WAP to delete first Node
+#include<iostream>
+using namespace std;
+class Node{   //Creating a Node
+   public:
+      int data;
+      Node *next;
+
+      Node(int data){
+        this->data = data;
+        next = NULL;
+        cout<<"\nNode is created";
+      }
+};
+//Deleting first position
+Node *deleteFirstNode(Node *head){
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+void display(Node *head){
+    Node *temp = head;
+    while(temp!=NULL){
+        cout<<"==>"<<temp->data;
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node *first = new Node(10);
+     Node *second = new Node(20);
+      Node *third = new Node(30);
+
+      Node *head = first;
+      //Connecting first node to second node
+      head->next = second;
+      //Connecting second node to third node
+      head->next->next = third;
+      cout<<"\nSingly Linked list Data ";
+     display(head);
+     
+     head = deleteFirstNode(head);
+     cout<<"\n";
+     display(head);
+    return 0;
+}
