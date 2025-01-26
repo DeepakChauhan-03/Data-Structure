54c31d9d86ff061f254639f0a23da1ae368f742d
Deepak Chauhan <deepakchauhan6141@gmail.com>
2025-01-26 23:39:08 +0530

    Constructor Node

---
+++ Linked List 1/NodeConstructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Node{
+  public:
+  int val;
+  Node *next;
+
+  Node(int val){
+    this->val = val;
+    this->next = NULL;
+    cout<<"Linked list is created"<<endl;
+  }
+};
+int main(){
+    Node a(10);
+    Node b(20);
+    Node c(30);
+    Node d(40);
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+
+    return 0;
+}
