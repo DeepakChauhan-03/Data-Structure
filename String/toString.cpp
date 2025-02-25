f8e6e90c0f609ceffb5d724019542a65aaffdfd9
Deepak Chauhan <deepakchauhan6141@gmail.com>
2025-02-25 19:08:15 +0530

    String to int

---
+++ String/toString.cpp
@@ -0,0 +1,9 @@
+//WAP to convert a integer value into string
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 123;
+    string s = to_string(x);
+    cout<<s;
+    return 0;
+}
---
+++ String/tointeger.cpp
@@ -0,0 +1,9 @@
+//WAP to convert a string into integer
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "123";
+    int x = stoi(s);
+    cout<<s;
+    return 0;
+}
