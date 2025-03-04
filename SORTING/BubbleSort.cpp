02b296969d6d297fd406a2e496bdbaa2765cc8f5
Deepak Chauhan <deepakchauhan6141@gmail.com>
2025-03-04 22:27:28 +0530

    Bubble sort

--- SORTING/BubbleSort.cpp
+++ SORTING/BubbleSort.cpp
@@ -1,29 +1,29 @@
 //WAP for bubble sort
 #include<iostream>
+#include<vector>
 using namespace std;
-void bubbleSort(int arr[], int s){
-    for(int i=0; i<s; i++){
-        for(int j = i+1; j<s; j++){
-            if(arr[i]>arr[j]){
-                int t = arr[i];
-                arr[i] = arr[j];
-                arr[j] = t;
+int main(){
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    vector <int> arr(n);
+    cout<<"Enter the elements : "<<endl;
+    for(int i = 0; i<arr.size(); i++){
+        cin>>arr[i];
+    }
+    for(int i = 0; i<n-1; i++){
+        for(int j = 0; j<n-1; j++){
+            if(arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
-}
-void show(int arr[], int s){
-    for(int i = 0; i<s; i++){
+    cout<<"Sorted elements are : ";
+    for(int i = 0; i<n; i++){
         cout<<arr[i]<<" ";
     }
-    cout<<endl;
-}
-int main()
-{
-    int arr[] = {11,2,3,44,55,2,4,6,88,4,99,7,8,5};
-    int n = sizeof(arr)/4;
-    show( arr, n);
-    bubbleSort(arr, n);
-    show(arr, n);
     return 0;
 }
